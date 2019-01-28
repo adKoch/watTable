2330a137d7e09a9f4a28e00d86164d9985b6e3f2
Adam Kochański <adam.kochanski.97@gmail.com>
2019-01-28 23:54:26 +0100

    Fix login screen

--- app/src/main/java/com/wat/student/adkoch/wattable/db/activities/MainActivity.java
+++ app/src/main/java/com/wat/student/adkoch/wattable/db/activities/MainActivity.java
@@ -3,6 +3,9 @@ package com.wat.student.adkoch.wattable.db.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
@@ -18,12 +21,23 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button loginButton;
+
     private static final int RC_SIGN_IN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton=findViewById(R.id.login_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createSignInIntent();
+            }
+        });
 
         FirebaseFirestore firestore = FirebaseFirestore.getInstance();
         FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
@@ -50,6 +64,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onRestart(){
+        super.onRestart();
+        AuthUI.getInstance().signOut(this);
+    }
+
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
@@ -60,8 +80,15 @@ public class MainActivity extends AppCompatActivity {
                 // Successfully signed in
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
                 Intent intent = new Intent(this, DayActivity.class);
+                Toast.makeText(this,"Pomyślnie zalogowano!",Toast.LENGTH_SHORT).show();
                 startActivity(intent);
             } else {
+
+                if(null==response){
+                    Toast.makeText(this,"Logowanie przerwano",Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this,"Błąd logowania",Toast.LENGTH_SHORT).show();
+                }
                 // Sign in failed. If response is null the user canceled the
                 // sign-in flow using the back button. Otherwise check
                 // response.getError().getErrorCode() and handle the error.
